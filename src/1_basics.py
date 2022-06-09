1ce78eab0819110b09d400c40007f42d622d6fdd
Александр Халиков <sashakhalikov@MacBook-Air-Aleksandr.local>
2022-06-09 21:47:09 +0300

    1 and 2

---
+++ src/1_basics.py
@@ -0,0 +1,7 @@
+import time
+
+# Nearly all programming languages, including Python, incorporate the concept of Unix time.
+# Python’s standard library includes a module called time that can
+# print the number of seconds since the Unix epoch:
+
+print(f'current time in UNIX format: {time.time()}')
---
+++ src/2_datetime_instances.py
@@ -0,0 +1,12 @@
+# The three classes that represent dates and times in datetime have similar initializers.
+# They can be instantiated by passing keyword arguments for each of the attributes,
+# such as year, date, or hour. You can try the code below to get a sense of how each object is created:
+
+from datetime import date, time, datetime
+print(date(year=2020, month=1, day=31))
+
+print(time(hour=13, minute=14, second=31))
+
+print(datetime(year=2020, month=1, day=31, hour=13, minute=14, second=31))
+
+print(f'With f-strings {date.today():%m/%d/%Y}')
